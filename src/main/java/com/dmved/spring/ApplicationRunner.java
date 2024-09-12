0c582b2cd6a7824e3a809bd0bb5d46ed7cc74829
Alina Abramova <alina.abramova@gmail.com>
2024-09-12 15:06:43 +0200

    xml configuration

--- src/main/java/com/dmved/spring/ApplicationRunner.java
+++ src/main/java/com/dmved/spring/ApplicationRunner.java
@@ -1,25 +1,12 @@
 package com.dmved.spring;
 
 import com.dmved.spring.database.pool.ConnectionPool;
-import com.dmved.spring.database.repository.CompanyRepository;
-import com.dmved.spring.database.repository.UserRepository;
-import com.dmved.spring.ioc.Container;
-import com.dmved.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        Container container = new Container();
-
-//        ConnectionPool connectionPool = new ConnectionPool();
-//        UserRepository userRepository = new UserRepository(connectionPool);
-//        CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-//        UserService userService = new UserService(userRepository, companyRepository);
-
-//        ConnectionPool connectionPool = container.get(ConnectionPool.class);
-//        UserRepository userRepository = container.get(UserRepository.class);
-//        CompanyRepository companyRepository = container.get(CompanyRepository.class);
-        UserService userService = container.get(UserService.class);
-        //todo userService use
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        System.out.println(context.getBean("pool2",ConnectionPool.class));
     }
 }
