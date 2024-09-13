96efa16c25e8d342ef9ce178f9abf07e7876b397
Alina Abramova <alina.abramova@gmail.com>
2024-09-13 12:56:56 +0200

    lesson 7 : xml configuration constructor injection

--- src/main/java/com/dmved/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmved/spring/database/pool/ConnectionPool.java
@@ -1,5 +1,18 @@
 package com.dmved.spring.database.pool;
 
-public class ConnectionPool {
+import java.util.List;
+import java.util.Map;
 
+public class ConnectionPool {
+    private final String username;
+    private final Integer poolSize;
+    private final List<Object> args;
+    private final Map<String, Object> properties;
+
+    public ConnectionPool(String username, Integer poolSize, List<Object> args, Map<String, Object> properties) {
+        this.username = username;
+        this.poolSize = poolSize;
+        this.args = args;
+        this.properties = properties;
+    }
 }
