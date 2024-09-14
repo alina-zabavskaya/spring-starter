a0c24ff84c9b80bbc4f40578791a4a2c65252af8
Alina Abramova <alina.abramova@gmail.com>
2024-09-14 19:51:49 +0200

    lesson 8 : xml configuration property injection

--- src/main/java/com/dmved/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/dmved/spring/database/pool/ConnectionPool.java
@@ -7,12 +7,16 @@ public class ConnectionPool {
     private final String username;
     private final Integer poolSize;
     private final List<Object> args;
-    private final Map<String, Object> properties;
+    private Map<String, Object> properties;
 
     public ConnectionPool(String username, Integer poolSize, List<Object> args, Map<String, Object> properties) {
         this.username = username;
         this.poolSize = poolSize;
         this.args = args;
+        this.properties = properties;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
     }
 }
