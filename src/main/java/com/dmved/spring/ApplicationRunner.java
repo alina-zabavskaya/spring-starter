301897378f1ff39843cc5239e0a6ac0edf5a300a
Alina Abramova <alina.abramova@gmail.com>
2024-09-14 19:36:25 +0200

    lesson 7 : xml configuration method injection

--- src/main/java/com/dmved/spring/ApplicationRunner.java
+++ src/main/java/com/dmved/spring/ApplicationRunner.java
@@ -1,12 +1,16 @@
 package com.dmved.spring;
 
 import com.dmved.spring.database.pool.ConnectionPool;
+import com.dmved.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        System.out.println(context.getBean("pool2",ConnectionPool.class));
+        System.out.println(context.getBean("pool1",ConnectionPool.class));
+
+        CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
