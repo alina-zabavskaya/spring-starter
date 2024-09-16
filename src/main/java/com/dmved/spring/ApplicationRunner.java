38df1e7703992a2d4cd4771870f070b9f266e6fc
Alina Abramova <alina.abramova@gmail.com>
2024-09-16 13:09:10 +0200

    lesson 12 : xml configuration BeanFactoryPostProcessor

--- src/main/java/com/dmved/spring/ApplicationRunner.java
+++ src/main/java/com/dmved/spring/ApplicationRunner.java
@@ -2,11 +2,19 @@ package com.dmved.spring;
 
 import com.dmved.spring.database.pool.ConnectionPool;
 import com.dmved.spring.database.repository.CompanyRepository;
+import java.io.Serializable;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
+
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
             //clazz -> String -> Map<String,Object>
             System.out.println(context.getBean("pool1", ConnectionPool.class));
