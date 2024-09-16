e3d29335bba5aebfbab1e6efea6a3623a3b08d96
Alina Abramova <alina.abramova@gmail.com>
2024-09-16 21:31:17 +0200

    lesson 16 : annotation-based configuration CustomBeanFactoryPostProcessor

--- src/main/java/com/dmved/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmved/spring/database/repository/CompanyRepository.java
@@ -1,15 +1,12 @@
 package com.dmved.spring.database.repository;
 
+import com.dmved.spring.bpp.InjectBean;
 import com.dmved.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
-    ConnectionPool connectionPool;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 
-    public static CompanyRepository of(ConnectionPool connectionPool){
-        return new CompanyRepository(connectionPool);
-    }
+
 }
