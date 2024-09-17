05b8e382f51cb3b6e11d726324966c1f5edc5760
Alina Abramova <alina.abramova@gmail.com>
2024-09-17 23:42:59 +0200

    lesson 34 : @SpringBootApplication

--- src/main/java/com/dmved/spring/ApplicationRunner.java
+++ src/main/java/com/dmved/spring/ApplicationRunner.java
@@ -2,11 +2,13 @@ package com.dmved.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationRunner.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(ApplicationRunner.class, args);
+        System.out.println(context.getBeanDefinitionCount());
     }
 }
--- src/main/java/com/dmved/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmved/spring/config/ApplicationConfiguration.java
@@ -1,34 +1,18 @@
 package com.dmved.spring.config;
 
 import com.dmved.spring.database.pool.ConnectionPool;
-import com.dmved.spring.database.repository.CrudRepository;
 import com.dmved.spring.database.repository.UserRepository;
 import com.dmved.web.config.WebConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
-//@ImportResource("classpath:application.xml")
 @Import(WebConfiguration.class)
 @Configuration(proxyBeanMethods = true)
-@PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = "com.dmved.spring",
-               useDefaultFilters = false,
-               includeFilters = {
-                    @Filter(type = FilterType.ANNOTATION, value = Component.class),
-                    @Filter(type = FilterType.ASSIGNABLE_TYPE, value = CrudRepository.class),
-                    @Filter(type = FilterType.REGEX, pattern = "com\\..+Repitory")
-
-               })
 public class ApplicationConfiguration {
         @Bean
         @Scope(BeanDefinition.SCOPE_SINGLETON)
