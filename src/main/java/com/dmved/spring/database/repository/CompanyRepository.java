ccec54b3f18a2074a2271beb3c976a755463a1e9
Alina Abramova <alina.abramova@gmail.com>
2024-09-17 10:18:43 +0200

    lesson 22 : annotation-based @Scope

--- src/main/java/com/dmved/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmved/spring/database/repository/CompanyRepository.java
@@ -1,19 +1,18 @@
 package com.dmved.spring.database.repository;
 
 import com.dmved.spring.bpp.Auditing;
-import com.dmved.spring.bpp.InjectBean;
 import com.dmved.spring.bpp.Transaction;
 import com.dmved.spring.database.entity.Company;
 import com.dmved.spring.database.pool.ConnectionPool;
 import java.util.List;
 import java.util.Optional;
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Repository
 @Transaction
 @Auditing
--- src/main/java/com/dmved/spring/database/repository/UserRepository.java
+++ src/main/java/com/dmved/spring/database/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.dmved.spring.database.repository;
 
 import com.dmved.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Repository
 public class UserRepository {
 
