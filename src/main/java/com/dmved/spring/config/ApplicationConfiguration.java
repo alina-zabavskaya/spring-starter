919940ea62ba3aa38810b5f49d1be544935a53fc
Alina Abramova <alina.abramova@gmail.com>
2024-09-17 12:50:13 +0200

    lesson 26 : java-based configuration @Bean

--- src/main/java/com/dmved/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmved/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,19 @@
 package com.dmved.spring.config;
 
+import com.dmved.spring.database.pool.ConnectionPool;
 import com.dmved.spring.database.repository.CrudRepository;
+import com.dmved.spring.database.repository.UserRepository;
 import com.dmved.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //@ImportResource("classpath:application.xml")
@@ -23,5 +29,14 @@ import org.springframework.stereotype.Component;
 
                })
 public class ApplicationConfiguration {
+        @Bean
+        @Scope(BeanDefinition.SCOPE_SINGLETON)
+        public ConnectionPool pool2(@Value("${db.username}") String username){
+            return new ConnectionPool(username, 20);
+        }
 
+        @Bean
+        public UserRepository userRepository(ConnectionPool pool2){
+            return new UserRepository(pool2);
+        }
 }
