d9836b7ba116c94784e7a4d59806fc622abdfa6e
Alina Abramova <alina.abramova@gmail.com>
2024-10-22 16:09:41 +0200

    lesson 43: Integration Test add configs and annotation

---
+++ src/test/java/com/dmved/spring/integration/annatation/IT.java
@@ -0,0 +1,16 @@
+package com.dmved.spring.integration.annatation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@SpringBootTest
+public @interface IT {
+
+}
--- src/test/java/com/dmved/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/dmved/spring/integration/service/CompanyServiceIT.java
@@ -3,22 +3,22 @@ package com.dmved.spring.integration.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.dmved.spring.config.DatabaseProperties;
 import com.dmved.spring.dto.CompanyReadDto;
+import com.dmved.spring.integration.annatation.IT;
 import com.dmved.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-//@ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = ApplicationRunner.class,
-//                      initializers = ConfigDataApplicationContextInitializer.class)
+@IT
+@RequiredArgsConstructor
+//@TestConstructor(autowireMode = AutowireMode.ALL)
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    CompanyService companyService;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById(){
