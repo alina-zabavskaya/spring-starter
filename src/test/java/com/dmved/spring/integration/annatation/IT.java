03ef25e2366cdc92291b54eea41fa74754bc4130
Alina Abramova <alina.abramova@gmail.com>
2024-10-23 22:42:03 +0200

    lesson 52: Move @Transactional to annotation @IT

--- src/test/java/com/dmved/spring/integration/annatation/IT.java
+++ src/test/java/com/dmved/spring/integration/annatation/IT.java
@@ -7,10 +7,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest(classes = TestApplicationRunner.class)
 public @interface IT {
 
--- src/test/java/com/dmved/spring/integration/repository/CompanyRepositoryTest.java
+++ src/test/java/com/dmved/spring/integration/repository/CompanyRepositoryTest.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.support.TransactionTemplate;
 
 @IT
 @RequiredArgsConstructor
-//@Transactional
 //@Commit
 class CompanyRepositoryTest {
 
